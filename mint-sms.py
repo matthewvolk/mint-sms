bcc6b337d3fd1fe99655de9b9407f96f59c4ede9
matthewvolk <volkmattj@gmail.com>
2018-03-29 22:05:03 -0700

    add two more print statements to print the value of first array object's accountName and fiLoginDisplayName

--- mint-sms.py
+++ mint-sms.py
@@ -6,4 +6,6 @@ mint_password = input('Please enter your Mint password: ')
 mint = mintapi.Mint(mint_username, mint_password)
 account_json = mint.get_accounts()
 
-print(account_json[0]['currentBalance'])
+print(account_json[0]['accountName'], end=' - ')
+print(account_json[0]['fiLoginDisplayName'])
+print((account_json[0]['currentBalance']))
