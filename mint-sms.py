8c1b3898a2d9d12ae5804e9ac3e7f78cd27b74f3
matthewvolk <volkmattj@gmail.com>
2018-03-29 21:59:00 -0700

    store the return value of an instance of the Mint class passing username and password values entered by user as arguments

--- mint-sms.py
+++ mint-sms.py
@@ -2,3 +2,5 @@ import mintapi
 
 mint_username = input('Please enter your Mint username: ')
 mint_password = input('Please enter your Mint password: ')
+
+mint = mintapi.Mint(mint_username, mint_password)
